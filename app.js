a441698451655ba6a47aacbf2cbd5d3f4d303a6b
pneumatick <dev@pop-os.localdomain>
2024-04-19 17:56:37 -0400

    Login complete

--- app.js
+++ app.js
@@ -27,6 +27,33 @@ app.post('/query', async (req, res) => {
 	});
 });
 
+app.post('/login', async (req, res) => {
+	let { username, password } = req.body;
+	let query = `SELECT COUNT(*) FROM \`user\` WHERE username='${username}' AND password='${password}';`;
+	con.query(query, function(err, result) {
+        if (err) {
+            res.status(404).json();
+            throw err;
+        }
+		let qResult = Object.values(JSON.parse(JSON.stringify(result)));
+		qResult.forEach((k, v) => {
+			if (k['COUNT(*)'] == 0) {
+				res.status(404).json();
+				res.send();
+			}
+		});
+    });
+
+	let char_query = `SELECT player_character.* FROM user INNER JOIN plays ON user.username = plays.username INNER JOIN player_character ON plays.character_ID=player_character.character_ID WHERE user.username='${username}';`;
+	con.query(char_query, function(err, result) {
+		if (err) {
+			res.status(404).json();
+			throw err;
+		}	
+		res.send({ characters: result });
+	});
+});
+
 
 app.listen(PORT, () => {
 	console.log(`Server is listening on port ${PORT}`);
--- public/main.js
+++ public/main.js
@@ -1,10 +1,18 @@
 const testButton = document.getElementById('test');
+const loginButton = document.getElementById('loginButton');
+const loginText = document.getElementById('username');
+const passText = document.getElementById('password');
+const loginDiv = document.getElementById('loginDiv');
+const charSelDiv = document.getElementById('characterSelectDiv');
+
+let username = '';
+let char_id = '';
 
 // This is the site that we will use to host the server.
-const URL = 'https://csc436-social-media-rpg.onrender.com';
+//const URL = 'https://csc436-social-media-rpg.onrender.com';
 
 // Uncomment this if you're testing on your own machine:
-//const URL = 'http://localhost:3000';
+const URL = 'http://localhost:3000';
 
 // Test function to get items
 async function itemQuery() {
@@ -33,4 +41,54 @@ async function itemQuery() {
 	});
 }
 
+// Login function
+async function login() {
+	let username = loginText.value;
+	let password = passText.value;
+	let response = await fetch(URL + '/login', {
+			method: "POST",
+			headers: {
+				'Accept': 'application/json',
+		    	'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				username: username,
+				password: password
+			})
+		})
+		.then(res => {
+			if (res.status === 200) {
+				console.log('Login successful');
+				loginDiv.parentNode.removeChild(loginDiv);
+				return res.json();
+			}
+				
+		})
+		.catch(err => { console.log(err); });
+	
+	response.characters.forEach((row) => {
+		let charDiv = document.createElement('div');
+		charDiv.id = row.character_ID;
+
+		let characterElem = document.createElement('h2');
+		characterElem.innerHTML = row.name;
+		charDiv.appendChild(characterElem);
+		
+		let charButton = document.createElement('button');
+		charButton.onclick = charSelect;
+		charButton.innerText = 'Select';
+		charDiv.appendChild(charButton);
+		
+		charSelDiv.appendChild(charDiv);
+	});
+}
+
+// Character select function
+function charSelect(elem) {
+	char_ID = elem.target.parentNode.id;
+	console.log("Char ID selected: ", char_ID);
+	charSelDiv.parentNode.removeChild(charSelDiv);
+}
+
 testButton.onclick = itemQuery;
+loginButton.onclick = login;
