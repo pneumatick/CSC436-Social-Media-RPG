5ed98009cdd6a971e1a07bd6e9fb817a8f3ba868
jakeelvin <165926425+jakeelvin@users.noreply.github.com>
2024-04-24 08:22:04 -0400

    Base Functionality

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const morgan = require('morgan');
-const cors = require('cors');
 var mysql = require('mysql');
 
 const {sanitizeString} = require('./helpers.js');
@@ -26,8 +25,7 @@ function handleDisconnect() {
 		console.log('Database error:', err);
 		if(err.code === 'PROTOCOL_CONNECTION_LOST') {
 			handleDisconnect();
-		} 
-		else {
+		} else {
 			throw err;
 		}
 	});
@@ -38,7 +36,6 @@ handleDisconnect();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.static('public'));
@@ -46,7 +43,7 @@ app.use(express.static('public'));
 // Handle front-end queries
 app.post('/query', async (req, res) => {
 	let {query} = req.body;
-	try {
+	try{
 	con.query(query, function(err, result) {
 		//if (err) {
 		//	res.status(400).json();
@@ -57,13 +54,14 @@ app.post('/query', async (req, res) => {
 	}
 	catch (err) {
 		console.log(err);
-		res.status(400).json();
+		res.status(400).json;
 	}
 });
 
 // Handle log in requests
 app.post('/login', async (req, res) => {
 	let loggedIn = false;
+	let charSelected = false;
 	let { username, password } = req.body;
 
 	// Sanitize the input
@@ -97,86 +95,11 @@ app.post('/login', async (req, res) => {
 					throw err;
 				}	
 				res.send({ characters: result });
+				charSelected = true;
 			});
 		}
+
     });
-});
-
-// Handle sign up requests
-app.post('/signup', async (req, res) => {
-	let { username, password, email } = req.body;
-
-	// Sanitize the input
-	username = sanitizeString(username);
-	password = sanitizeString(password);
-	email = sanitizeString(email);
-
-	// Check if the user exists
-	let query = `SELECT COUNT(*) FROM \`user\` WHERE username='${username}';`;
-	con.query(query, function(err, result) {
-		if (err) {
-			res.status(404).json();
-			throw err;
-		}
-		let qResult = Object.values(JSON.parse(JSON.stringify(result)));
-		qResult.forEach((k, v) => {
-			if (k['COUNT(*)'] >= 1) {
-				console.log("Sign up attempt failed (user exists)");
-				res.status(409).json();
-				res.send();
-			}
-			else {
-				let insert_query = `INSERT INTO \`user\` (email, username, password) VALUES ('${email}', '${username}', '${password}');`;
-				con.query(insert_query, function(err, result) {
-					if (err) {
-						res.status(404).json();
-						throw err;
-					}
-					let new_id;
-					let id_query = `SELECT MAX(character_ID) FROM player_character;`;
-					con.query(id_query, function(err, result) {
-						if (err) {
-							res.status(404).json();
-							throw err;
-						}
-						let qResult = Object.values(JSON.parse(JSON.stringify(result)));
-						qResult.forEach((k, v) => {
-							new_id = k['MAX(character_ID)'] + 1;
-						});
-						let char_name = `Unknown Brute`;
-						let race = `human`;
-						let char_class = `fighter`;
-						let tool_p = `none`;
-						let weap_p = `melee weapons`;
-						let health = 10;
-
-						let char_query = `INSERT INTO player_character (character_ID, name, race, class, tool_proficiency, weapon_proficiency, health_points) VALUES (${new_id}, '${char_name}', '${race}', '${char_class}', '${tool_p}', '${weap_p}', ${health});`;
-						let plays_query = `INSERT INTO plays (username, character_ID) VALUES ('${username}', ${new_id});`;
-						let in_query = `INSERT INTO currently_in (character_ID, sublocation_ID) VALUES (${new_id}, 6);`
-						con.query(char_query, function(err, result) {
-							if (err) {
-								res.status(404).json();
-								throw err;
-							}
-						});
-						con.query(plays_query, function(err, result) {
-							if (err) {
-								res.status(404).json();
-								throw err;
-							}
-						});
-						con.query(in_query, function(err, result) {
-							if (err) {
-								res.status(404).json();
-								throw err;
-							}
-						});
-						res.send({ char_id: new_id });
-					});
-				});
-			}
-		});
-	});
 });
 
 app.listen(PORT, () => {
