b46a840734a8c2b4451b6f95224f92cc7f227be3
jakeelvin <165926425+jakeelvin@users.noreply.github.com>
2024-04-24 01:15:08 -0400

    Adding SubLoc Functionality and some Buttons

--- public/main.js
+++ public/main.js
@@ -5,23 +5,28 @@ const switchButton = document.getElementById('switchButton');
 
 const charSelectButton = document.getElementById('charSelectButton')
 const mapNavButton = document.getElementById('mapNavButton')
-const mapCloseButton = document.getElementById('mapCloseButton')
 
 const loginText = document.getElementById('username');
 const passText = document.getElementById('password');
 const loginDiv = document.getElementById('loginDiv');
 const charSelDiv = document.getElementById('characterSelectDiv');
+const mapNavDiv = document.getElementById('mapNavDiv')
+
+const subSwap = document.getElementById('subSwapDiv');
 
 const inventory = document.getElementById('inventoryList');
 
 let username = '';
 let char_id = '';
+let current_region = '';
+let current_subloc = '';
+let selected_subloc = '';
 
 // This is the site that we will use to host the server.
-const URL = 'https://csc436-social-media-rpg.onrender.com';
+//const URL = 'https://csc436-social-media-rpg.onrender.com';
 
 // Uncomment this if you're testing on your own machine:
-//const URL = 'http://localhost:3000';
+const URL = 'http://localhost:3000';
 
 // Test function to get items
 async function itemQuery() {
@@ -66,7 +71,7 @@ async function signUp() {
 	username = loginText.value;
 	let password = await sha256(passText.value);
 	let email = emailText.value;
-	let response = await fetch(URL + '/signup', {
+	let signResponse = await fetch(URL + '/signup', {
 			method: "POST",
 			headers: {
 				'Accept': 'application/json',
@@ -93,10 +98,12 @@ async function signUp() {
 		.catch(err => { console.log(err); });
 	
 	// Create a div for each character and a button to select them
-	if (response) {
-		char_id = response.char_id;
+	if (signResponse) {
+		char_id = signResponse.char_id;
 		console.log("Char ID selected: ", char_id);
 		fetchLocation();
+		fetchSublocation();
+		fetchSubList();
 		fetchCharacterInfo();
 		fetchInventory();
 	}
@@ -106,7 +113,7 @@ async function signUp() {
 async function login() {
 	username = loginText.value;
 	let password = await sha256(passText.value);
-	let response = await fetch(URL + '/login', {
+	let loginResponse = await fetch(URL + '/login', {
 			method: "POST",
 			headers: {
 				'Accept': 'application/json',
@@ -132,9 +139,9 @@ async function login() {
 		.catch(err => { console.log(err); });
 	
 	// Create a div for each character and a button to select them
-	if (response) {
-		console.log(response.characters)
-		response.characters.forEach((row) => {
+	if (loginResponse) {
+		console.log(loginResponse.characters)
+		loginResponse.characters.forEach((row) => {
 			let charDiv = document.createElement('div');
 			charDiv.id = row.character_ID;
 
@@ -158,6 +165,8 @@ function charSelect(elem) {
 	console.log("Char ID selected: ", char_id);
 	charSelDiv.parentNode.removeChild(charSelDiv);
 	fetchLocation();
+	fetchSublocation();
+	fetchSubList();
 	fetchCharacterInfo();
 	fetchInventory();
 }
@@ -180,14 +189,136 @@ function signOrLog(elem) {
 	}
 }
 
-function toggleMap(){
-	document.getElementById('mapNavDiv').classList.toggle('active')
+/* Nathaniel's Code */
+
+async function listChars(){
+    let response = await fetch(URL + '/query', {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({query: `SELECT char2.name 
+        FROM player_character JOIN plays ON player_character.character_ID = plays.character_ID JOIN user ON user.username = plays.username JOIN plays AS plays2 ON plays2.username = user.username JOIN player_character AS char2 ON plays2.character_ID = char2.character_ID 
+        WHERE player_character.character_ID = ${char_id}`})
+    })
+    .then(res => {
+        if (res.status === 200) {
+            return res.json();
+        }
+        else { return null }
+    })
+    .catch(err => { console.log(err); });
+
+    if (response) {
+        response.query.forEach((row) => {
+            console.log(row.name)
+            let charDiv = document.createElement('div');
+            charDiv.id = row.character_ID;
+
+            let characterElem = document.createElement('h2');
+            characterElem.innerHTML = row.name;
+            charDiv.appendChild(characterElem);
+
+            let charButton = document.createElement('button');
+            charButton.onclick = charSelect(row.character_ID);
+            charButton.innerText = 'Select';
+            charDiv.appendChild(charButton);
+
+            charSelDiv.appendChild(charDiv);
+        });
+    }
 }
 
 
+
+async function openMap(){
+    let response = await fetch(URL + '/query', {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({query: `SELECT connections.name FROM location JOIN connects_to ON location.location_ID = connects_to.location_ID JOIN location AS connections ON connections.location_ID = connects_to.location_ID_2 WHERE location.name = (SELECT location.name FROM player_character JOIN currently_in ON player_character.character_ID = currently_in.character_ID JOIN is_part_of ON is_part_of.sublocation_ID = currently_in.sublocation_ID JOIN location ON location.location_ID = is_part_of.location_ID WHERE player_character.character_ID = ${char_id}) UNION SELECT connections.name FROM location JOIN connects_to ON location.location_ID = connects_to.location_ID_2 JOIN location AS connections ON connections.location_ID = connects_to.location_ID WHERE location.name = (SELECT location.nameFROM player_character JOIN currently_in ON player_character.character_ID = currently_in.character_ID JOIN is_part_of ON is_part_of.sublocation_ID = currently_in.sublocation_ID JOIN location ON location.location_ID = is_part_of.location_ID WHERE player_character.character_ID = ${char_id})`})
+    })
+    .then(res => {
+        if (res.status === 200) {
+            return res.json();
+        }
+        else { return null }
+    })
+    .catch(err => { console.log(err); });
+
+    if(response){
+        let mapCloseButton = document.createElement('button');
+        mapCloseButton.innerText = "Close";
+        mapCloseButton.onclick = closeMap;
+        mapNavDiv.appendChild(mapCloseButton)
+
+        let mapTitle = document.createElement('h2');
+        mapTitle.innerText = "Map of Salamar and the Kendakrath Mountains";
+        mapNavDiv.appendChild(mapTitle)
+
+        let mapImg = document.createElement('img');
+        mapImg.src = "images/Social_Media_RPG_Map_w_Labels.jpeg";
+        mapImg.alt = "map";
+        mapImg.width = "800";
+        mapImg.height = "450";
+        mapNavDiv.appendChild(mapImg)
+
+        let navText = document.createElement('h3');
+        navText.innerText = "Travel to:";
+        mapNavDiv.appendChild(navText)
+
+        response.query.forEach((row) => {
+            console.log(row.name);
+            let navContainer = document.createElement('div');
+            navContainer.id = row.name;
+
+            let locButton = document.createElement('button');
+            locButton.onclick = navToLoc(row.name);
+            locButton.innerText = row.name;
+            mapNavDiv.appendChild(locButton);
+
+
+        });
+
+    }
+}
+
+async function navToLoc(locname){
+    let response = await fetch(URL + '/query', {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({query: `UPDATE currently_in SET currently_in.character_ID = (SELECT character_ID FROM player_character WHERE character_ID = ${char_id}), currently_in.sublocation_ID = (SELECT is_part_of.sublocation_ID FROM is_part_of JOIN location ON is_part_of.location_ID = location.location_ID WHERE location.name = ${locname} LIMIT 1) WHERE currently_in.character_ID = (SELECT character_ID FROM player_character WHERE character_ID = ${char_id});`})
+    })
+    .then(res => {
+        if (res.status === 200) {
+            return res.json();
+        }
+        else { return null }
+    })
+    .catch(err => { console.log(err); });
+
+    if (response){
+        closeMap()
+    }
+}
+
+function closeMap(){
+    mapNavDiv.innerHTML = "";
+    //document.getElementById(mapNavDiv).classList.toggle("active");
+}
+
+
+
 /* Jake's Code */
+// Fill the Location Header
 async function fetchLocation() {
-	let response = await fetch(URL + '/query', {
+	let locResponse = await fetch(URL + '/query', {
 		method: "POST",
 		headers: {
 			'Accept': 'application/json',
@@ -198,24 +329,93 @@ async function fetchLocation() {
 	.then(res => res.json())
 	.catch(err => { console.log(err); return null; });
 
-    if (response) {
-        let locationData = response.query[0];
+    if (locResponse) {
+        let locationData = locResponse.query[0];
+
+    	current_region = locationData.region;
+
 		document.getElementById('locationDiv').style.backgroundColor = 'cornsilk';
         document.getElementById('locName').innerHTML = locationData.name;
-        document.getElementById('locType').innerHTML = locationData.location_type + ', ' + locationData.region;
-        document.getElementById('locDesc').innerHTML = 'Description: ' + locationData.description;
+        document.getElementById('locType').innerHTML = locationData.region + ', ' + locationData.location_type;
+        document.getElementById('locDesc').innerHTML = locationData.description;
 	}
 }
 
+// Fill the Sublocation Header
+async function fetchSublocation() {
+	let subResponse = await fetch(URL + '/query', {
+		method: "POST",
+		headers: {
+			'Accept': 'application/json',
+	    	'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({query: `SELECT sublocation.* FROM player_character JOIN currently_in ON player_character.character_ID = currently_in.character_ID JOIN sublocation ON currently_in.sublocation_ID = sublocation.sublocation_ID WHERE player_character.character_ID = ${char_id}`})
+	})
+	.then(res => res.json())
+	.catch(err => { console.log(err); return null; });
+
+    if (subResponse) {
+        let sublocData = subResponse.query[0];
+        document.getElementById('subName').innerHTML = sublocData.name;
+        document.getElementById('subType').innerHTML = sublocData.building_type;
+        document.getElementById('subDesc').innerHTML = sublocData.description;
+
+        current_subloc = sublocData.name;
+	}
+}
+
+// Create the Sublocation Button List
+async function fetchSubList() {
+	let subListResponse = await fetch(URL + '/query', {
+		method: "POST",
+		headers: {
+			'Accept': 'application/json',
+	    	'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({query: `SELECT sublocation.* FROM player_character JOIN currently_in ON player_character.character_ID = currently_in.character_ID JOIN is_part_of ON currently_in.sublocation_ID = is_part_of.sublocation_ID JOIN location ON is_part_of.location_ID = location.location_ID JOIN is_part_of AS others_part_of ON location.location_ID = others_part_of.location_ID JOIN sublocation ON others_part_of.sublocation_ID = sublocation.sublocation_ID WHERE player_character.character_ID =  ${char_id} AND sublocation.name != '${current_subloc}'` })
+	})
+	.then(res => res.json())
+	.catch(err => { console.log(err); return null; });
+
+    if (subListResponse) {
+        document.getElementById('subSwapInfo').textContent = 'Available Travel Locations within ' + current_region + ':';
+        subListResponse.query.forEach((row) => {
+        	let subDiv = document.createElement('div')
+
+            let subButton = document.createElement('button');
+            subButton.onclick = 'subSelect(row.name)';
+            subButton.innerText = row.name;
+			subDiv.appendChild(subButton);
+
+            subSwap.appendChild(subDiv);
+		});
+    }
+}
+
+// Sublocation select function
+function subSelect(name) { 
+/*	let selectResponse = fetch(URL + '/query', {
+		method: "POST",
+		headers: {
+			'Accept': 'application/json',
+	    	'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({query: `UPDATE currently_in SET sublocation_ID = ( SELECT sublocation_ID FROM sublocation WHERE sublocation.name = '${name}') WHERE currently_in.character_ID = ${char_id} `})
+	})
+	.then(res => res.json())
+	.catch(err => { console.log(err); return null; });
 
 
+	fetchSublocation();
+	console.log('SubSelect Error') */
+}
 
 
 
 /* Anish's Code */
 // Fetch character information using character ID
 async function fetchCharacterInfo() {
-    let response = await fetch(URL + '/query', {
+    let charResponse = await fetch(URL + '/query', {
         method: "POST",
         headers: {
             'Accept': 'application/json',
@@ -228,9 +428,9 @@ async function fetchCharacterInfo() {
     .then(res => res.json())
     .catch(err => { console.error(err); return null; });
 
-    if (response) {
+    if (charResponse) {
         // Assuming response.character is the data received for character
-        let characterData = response.query[0];
+        let characterData = charResponse.query[0];
 		document.getElementById('characterDiv').style.backgroundColor = 'cornsilk';
         document.getElementById('charInfo').textContent = 'Character Info:';
         document.getElementById('charName').textContent = characterData.name;
@@ -245,7 +445,7 @@ async function fetchCharacterInfo() {
 
 // Fetch inventory items using character ID
 async function fetchInventory() {
-    let response = await fetch(URL + '/query', {
+    let invResponse = await fetch(URL + '/query', {
         method: "POST",
         headers: {
             'Accept': 'application/json',
@@ -256,15 +456,18 @@ async function fetchInventory() {
     .then(res => res.json())
     .catch(err => { console.error(err); return null; });
 
-    if (response) {
-        let inventoryList = document.getElementById('inventoryList');
-        inventoryList.innerHTML = ''; // Clear existing items
+    if (invResponse) {
+		document.getElementById('inventory').style.backgroundColor = 'cornsilk';
+        document.getElementById('invInfo').textContent = 'Inventory:';
+
+        let inventory = document.getElementById('inventoryList');
+        inventory.innerHTML = ''; // Clear existing items
 
         // Create a list item for each item in the inventory
-        response.query.forEach(item => {
+        invResponse.query.forEach(item => {
 			console.log(item.type)
             let listItem = document.createElement('li');
-            listItem.textContent = item.type;
+            listItem.textContent = '	' + item.type;
             inventory.appendChild(listItem);
         });
     }
@@ -275,8 +478,8 @@ async function fetchInventory() {
 //testButton.onclick = itemQuery;
 loginButton.onclick = login;
 switchButton.onclick = signOrLog;
-mapNavButton.onclick = toggleMap;
-mapCloseButton.onclick = toggleMap;
+mapNavButton.onclick = openMap;
+charSelectButton.onclick = listChars;
 
 // Assign functions to elements
 testButton.onclick = itemQuery;
